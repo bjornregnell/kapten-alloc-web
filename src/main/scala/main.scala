93f658fd96bf1e2a6efb1fe2744b469d99a18605
bjornregnell <bjorn.regnell@cs.lth.se>
2021-06-22 21:26:57 +0200

    removed unused method

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,9 @@ package kaptenallocweb
 import org.scalajs.dom
 import org.scalajs.dom.document
 
+object Main:
+  def main(args: Array[String]): Unit = 
+    document.addEventListener("DOMContentLoaded", (e: dom.Event) => setupUI())  
 
 extension (s: String)
   def containsAll(xs: Array[String], isCaseSensitive: Boolean = false): Boolean =
@@ -10,43 +13,33 @@ extension (s: String)
     val s2 = if isCaseSensitive then s else s.toLowerCase
     xs2.forall(x => s2.contains(x))
 
-  def containsAtLeastOne(xs: Array[String], isCaseSensitive: Boolean = false): Boolean = 
-    val xs2 = if isCaseSensitive then xs else xs.map(_.toLowerCase)
-    val s2 = if isCaseSensitive then s else s.toLowerCase
-    xs2.exists(x => s2.contains(x))
-
 extension (rows: Seq[String])
   def filterRows(words: Array[String]): Seq[String] = 
     for row <- rows 
     if row.containsAll(words) || row.startsWith("---") || row.startsWith("kurs")
     yield row
 
-object Main:
-  def appendPar(targetNode: dom.Node, text: String): dom.html.Paragraph = 
-    val parNode = document.createElement("p").asInstanceOf[dom.html.Paragraph]
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
-    parNode
+def appendPar(targetNode: dom.Node, text: String): dom.html.Paragraph = 
+  val parNode = document.createElement("p").asInstanceOf[dom.html.Paragraph]
+  parNode.textContent = text
+  targetNode.appendChild(parNode)
+  parNode
 
-  def main(args: Array[String]): Unit = 
-    println("Kapten Alloc finns i konsollen!")
-    document.addEventListener("DOMContentLoaded", (e: dom.Event) => setupUI())  
+def setupUI(): Unit = 
+  val input = document.createElement("input").asInstanceOf[dom.html.Input]
+  val showText = document.createElement("pre").asInstanceOf[dom.html.Pre]
+  showText.textContent = dataGeneratedFromKaptenAlloc.mkString("\n")
+  val filterText = appendPar(document.body, "Filter: ")
 
-  def setupUI(): Unit = 
-    val input = document.createElement("input").asInstanceOf[dom.html.Input]
-    val showText = document.createElement("pre").asInstanceOf[dom.html.Pre]
-    showText.textContent = dataGeneratedFromKaptenAlloc.mkString("\n")
-    val filterText = appendPar(document.body, "Filter: ")
+  input.id = "myInput"
+  input.classList.add("input"); 
+  input.setAttribute("value", "")
 
-    input.id = "myInput"
-    input.classList.add("input"); 
-    input.setAttribute("value", "")
+  input.addEventListener("input", { (e: dom.Event) =>
+    val words = input.value.split(' ')
+    showText.textContent = 
+      dataGeneratedFromKaptenAlloc.filterRows(words).mkString("\n")
+  })
 
-    input.addEventListener("input", { (e: dom.Event) =>
-      val words = input.value.split(' ')
-      showText.textContent = 
-        dataGeneratedFromKaptenAlloc.filterRows(words).mkString("\n")
-    })
-
-    filterText.appendChild(input)
-    document.body.appendChild(showText)
+  filterText.appendChild(input)
+  document.body.appendChild(showText)

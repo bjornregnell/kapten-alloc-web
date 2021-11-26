0389e7482bc9a2c33843c815afcec23d463ae603
bjornregnell <bjorn.regnell@cs.lth.se>
2021-11-26 14:25:02 +0100

    swith build to fastLinkJS due to build err

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-sbt fullLinkJS
-scp target/scala-3.0.0/hello-scala-js-opt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
+sbt fastLinkJS
+scp target/scala-3.1.0/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
 scp index.html $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.

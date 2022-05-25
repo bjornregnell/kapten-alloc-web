67eef3c8f777370cdf3cbbd9e3aad7f9b6db61d0
bjornregnell <bjorn.regnell@cs.lth.se>
2022-05-25 16:49:42 +0200

    bump versions

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 enablePlugins(ScalaJSPlugin)
 
 name := "kaptenalloc-web"
-scalaVersion := "3.1.0" // or any other Scala version >= 2.11.12
+scalaVersion := "3.1.2" // or any other Scala version >= 2.11.12
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 sbt fastLinkJS
-scp target/scala-3.1.0/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
+scp target/scala-3.1.2/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
 scp index.html $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.

e4da4cfa04d27e9abc38ecdb993ae0515cf38dab
bjornregnell <bjorn.regnell@cs.lth.se>
2022-09-02 21:13:48 +0200

    run bump-version in publish (requires scala-cli)

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+scala-cli bump-version.sc
 sbt fastLinkJS
 scp target/scala-3.1.3/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
 scp index.html $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.

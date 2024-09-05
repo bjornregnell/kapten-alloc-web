94982f95797762f1c57524dabb5648cbfcdf5fdf
bjornregnell <bjorn.regnell@cs.lth.se>
2024-09-05 10:31:11 +0200

    fix bad hardcoded target dir :(

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 scala-cli bump-version.sc
 sbt fastLinkJS
-scp target/scala-3.1.3/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
+scp target/scala-3.5.0/kaptenalloc-web-fastopt/main.js $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
 scp index.html $LUCATID@fileadmin.cs.lth.se:pgk/kaptenalloc/.
